a40062be2430c3a99ae392fc7b53ed83e77f75d3
qayol1 <qayol@freemail.hu>
2017-06-14 14:45:26 +0200

    base game implemented

---
+++ PingPong/PingPong/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(50, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(20, 120);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(750, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(20, 120);
+            this.panel2.TabIndex = 1;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ball.Location = new System.Drawing.Point(400, 100);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(16, 18);
+            this.ball.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 312);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel ball;
+    }
+}
+
---
+++ PingPong/PingPong/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+
+        public int Bar1YPosition = 100;
+        public int Bar2YPosition = 100;
+        public int BallXPosition = 400;
+        public int BallYPosition = 100;
+        Boolean fromLeftToRight;
+        Boolean fromUpToDown;
+        Boolean paused;
+        Timer t;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartBall();
+        }
+
+        private void StartBall()
+        {
+            t = new Timer();
+            t.Interval = 20;
+            t.Tick += new EventHandler(MoveBall);
+            t.Start();
+            initDirection();
+        }
+
+        void initDirection()
+        {
+            Random rnd = new Random();
+            int rightLeft = rnd.Next(0, 2);
+
+            if (rightLeft == 0)
+            {
+                fromLeftToRight = true;
+            }
+            else
+            {
+                fromLeftToRight = false;
+            }
+
+            int upDown = rnd.Next(0, 2);
+
+            if (upDown == 0)
+            {
+                fromUpToDown = true;
+            }
+            else
+            {
+                fromUpToDown = false;
+            }
+        }
+
+
+        void MoveBall(object sender, EventArgs e)
+        {
+            if (fromLeftToRight)
+            {
+                if(BallXPosition<730)
+                {
+                    if (fromUpToDown)
+                    {
+                        if (BallYPosition < 290)
+                        {
+                            BallXPosition += 4;
+                            BallYPosition += 4;
+                            ball.Location = new Point(BallXPosition, BallYPosition);
+                        }
+                        else
+                        {
+                            fromUpToDown = false;
+                        }
+
+                    }
+                    else
+                    {
+                        if (BallYPosition > 4)
+                        {
+                            BallXPosition += 4;
+                            BallYPosition -= 4;
+                            ball.Location = new Point(BallXPosition, BallYPosition);
+                        }
+                        else
+                        {
+                            fromUpToDown = true;
+                        }
+
+                    }
+                }
+                if(BallXPosition>730 && (Bar2YPosition + 120 > BallYPosition && BallYPosition > Bar2YPosition))
+                {
+                    fromLeftToRight = false;
+                }
+                
+            }
+            else
+            {
+                if (BallXPosition > 70)
+                {
+                    if (fromUpToDown)
+                    {
+                        if (BallYPosition < 290)
+                        {
+                            BallXPosition -= 4;
+                            BallYPosition += 4;
+                            ball.Location = new Point(BallXPosition, BallYPosition);
+                        }
+                        else
+                        {
+                            fromUpToDown = false;
+                        }
+                    }
+                    else
+                    {
+                        if (BallYPosition > 4)
+                        {
+                            BallXPosition -= 4;
+                            BallYPosition -= 4;
+                            ball.Location = new Point(BallXPosition, BallYPosition);
+                        }
+                        else
+                        {
+                            fromUpToDown = true;
+                        }
+                    }
+                }
+                if (BallXPosition <70 && (Bar1YPosition + 120> BallYPosition && BallYPosition >Bar1YPosition))
+                {
+                    fromLeftToRight = true;
+                }
+            }
+                    
+        }
+
+        void StopBall()
+        {
+            t.Stop();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyValue == 81)
+            {
+                // Q key pressed
+                if (Bar1YPosition > 4)
+                {
+                    Bar1YPosition -= 4;
+                    panel1.Location = new Point(50, Bar1YPosition);
+                }
+
+            }
+            if (e.KeyValue == 65)
+            {
+                // A key pressed
+                if (Bar1YPosition < 185)
+                {
+                    Bar1YPosition += 4;
+                    panel1.Location = new Point(50, Bar1YPosition);
+                }
+
+            }
+
+            if (e.KeyValue == 80)
+            {
+                // P key pressed
+                if (Bar2YPosition > 4)
+                {
+                    Bar2YPosition -= 4;
+                    panel2.Location = new Point(750, Bar2YPosition);
+                }
+            }
+
+
+
+            if (e.KeyValue == 76)
+            {
+                // L key pressed
+                if (Bar2YPosition < 185)
+                {
+                    Bar2YPosition += 4;
+                    panel2.Location = new Point(750, Bar2YPosition);
+                }
+
+            }
+
+            if (e.KeyValue == 27)
+            {
+                // ESC key pressed
+                StopBall();
+                Application.Exit();
+
+
+            }
+
+            if (e.KeyValue == 32)
+            {
+                // SPACE key pressed
+                if(paused)
+                {
+                    t.Start();
+                    paused = false;
+                } else
+                {
+                    t.Stop();
+                    paused = true;
+                }
+                
+
+            }
+        }
+
+
+    }
+}
